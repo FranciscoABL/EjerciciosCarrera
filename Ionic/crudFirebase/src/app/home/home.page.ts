101e5be17ec0f1f9dbeaa83b3850fd234bf29e97
FranciscoABL <16171290@itculiacan.edu.mx>
2021-01-14 14:04:13 -0700

    agregando add,delete y update

--- Ionic/crudFirebase/src/app/home/home.page.ts
+++ Ionic/crudFirebase/src/app/home/home.page.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  task:string
+  taskList=[]
+  constructor(private navctrl:NavController,private alertctrl:AlertController) {}
+  ngOnInit(): void {
+    this.getAllTask()
+  }
 
-  constructor() {}
-
+  agregarTask(taskName){
+    if (taskName.value.length>0){
+      this.taskList.push({
+        title:taskName.value,
+        id: this.taskList.length +1 +""
+      });
+      taskName.value="";
+    }
+  }
+  deleteTask(i){
+    this.taskList.splice(i,1)
+  }
+  getAllTask(){
+    return [...this.taskList]
+  }
+ async updateTask(index){
+    let alert= await this.alertctrl.create({
+      header:'Actualizar tarea?',
+      message:'Escribe la nueva tarea',
+      inputs:[{
+        name:'Editar',placeholder:'Tarea'
+      }],
+      buttons:[{text:'Cancelar',role:'cancel'},{text:'Actualizar',
+        handler:data=>{
+          this.taskList[index]=data.Editar;
+        }
+      }]
+    });
+   await alert.present();
+  }
 }
