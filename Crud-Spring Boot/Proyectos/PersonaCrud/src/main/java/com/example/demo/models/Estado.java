e365128598b74f6a95273bbece852ea274733cca
FranciscoABL <16171290@itculiacan.edu.mx>
2021-08-08 17:19:13 -0600

    Practicando curso spring

---
+++ Crud-Spring Boot/Proyectos/PersonaCrud/src/main/java/com/example/demo/models/Estado.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Pako
+ */
+@Entity
+@Table(name="estado")
+public class Estado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="id_pais")
+    private Pais pais;
+    private String nombre;
+
+    public Estado(Pais pais, String nombre) {
+        this.pais = pais;
+        this.nombre = nombre;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public void setPais(Pais pais) {
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
